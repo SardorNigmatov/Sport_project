92f97b41b7cad99404490f4e1e8c1b1fff92a55c
samandarsaburov <saburovsamandar@gmail.com>
2023-09-09 18:21:40 +0500

    coach vieaws

--- sport/views.py
+++ sport/views.py
@@ -5,6 +5,23 @@ from .models import CoachModel
 from rest_framework.permissions import IsAuthenticated
 # Create your views here.
 
-class ListCoachViews(generics.ListAPIView):
+class CoachListViews(generics.ListAPIView):
     queryset = CoachModel.objects.all()
     serializer_class = CoachSerializer
+
+class CoachCreateViews(generics.CreateAPIView):
+    queryset = CoachModel.objects.all()
+    serializer_class = CoachSerializer
+    # permission_classes = (AutherUserPermissions,)
+
+    
+class CoachUpdateViews(generics.UpdateAPIView):
+    queryset = CoachModel.objects.all()
+    serializer_class = CoachSerializer
+    # permission_classes = (AdminPermission,)
+
+class CoachDeleteViews(generics.DestroyAPIView):
+    queryset = CoachModel.objects.all()
+    serializer_class = CoachSerializer
+    # permission_classes = (AdminPermission,)
+    
