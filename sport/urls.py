7266f986733369fbed79339594c6b0cb64317acc
samandarsaburov <saburovsamandar@gmail.com>
2023-09-11 13:37:37 +0500

    coach url

--- sport/urls.py
+++ sport/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
-from .views import ListCoachViews
+from .views import CoachListViews ,CoachCreateViews, CoachUpdateViews , CoachDeleteViews
 
 urlpatterns = [
-    path('coach-all/', ListCoachViews.as_view(), name='coach_all')
+    path('coach-all/', CoachListViews.as_view(), name='coach_all'),
+    path('coach-create/', CoachCreateViews.as_view(), name='coach_create'),
+    path('coach-update/<int:pk>', CoachUpdateViews.as_view(), name='coach_update'),
+    path('coach-delete/<int:pk>', CoachDeleteViews.as_view(), name='coach_delete'),
 ]
