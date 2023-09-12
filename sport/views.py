311f51a7377d03015133f42e977e79aa7975c0ab
samandarsaburov <saburovsamandar@gmail.com>
2023-09-12 15:17:01 +0500

    refresh

--- sport/views.py
+++ sport/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from rest_framework.views import APIView
-from .models import YangilikModels, Gyms
-from .serializers import YangilikSerializer, GymsSerializer, GymsCreateSerializer
+from .models import YangilikModels, Gyms , CoachModel
+from .serializers import YangilikSerializer, GymsSerializer, GymsCreateSerializer , CoachSerializer
 from rest_framework.response import Response
 from django.shortcuts import get_object_or_404
 from rest_framework.status import HTTP_201_CREATED
@@ -54,10 +54,6 @@ class DeleteYangilikView(APIView):
         return Response({'msg': 'deleted'})
 
 
-from rest_framework import generics
-from .serializers import CoachSerializer
-from .models import CoachModel
-from rest_framework.permissions import IsAuthenticated
 # Create your views here.
 
 class CoachListViews(generics.ListAPIView):
